b7aaa6dea43ead425b87ba534debd7f22e4a5be8
Ihor Ahnianikov <ahnianikov@gmail.com>
2020-01-16 23:42:19 +0200

    Fix compatibility

--- hivepress-paid-listings.php
+++ hivepress-paid-listings.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: HivePress Paid Listings
  * Description: Charge users for adding listings.
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivepress-paid-listings
@@ -14,10 +14,10 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Register plugin directory.
+// Register extension directory.
 add_filter(
-	'hivepress/v1/dirs',
-	function( $dirs ) {
-		return array_merge( $dirs, [ __DIR__ ] );
+	'hivepress/v1/extensions',
+	function( $extensions ) {
+		return array_merge( $extensions, [ __DIR__ ] );
 	}
 );
