23308dd6e2bce38d4e1d26fddd822549281fca44
Ihor Ahnianikov <ahnianikov@gmail.com>
2020-01-26 00:02:17 +0200

    Remove files

--- templates/listing-package/view/listing-package-description.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-?>
-<div class="hp-listing-package__description"><?php the_content(); ?></div>
--- templates/listing-package/view/listing-package-price.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-?>
-<div class="hp-listing-package__price"><?php echo ! empty( $product ) ? $product->get_price_html() : esc_html__( 'Free', 'hivepress-paid-listings' ); ?></div>
