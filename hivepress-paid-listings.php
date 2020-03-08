30bcda082dc21e28fcb2efcc65a6db29de1e8d52
Ihor <ihor@hivepress.io>
2020-03-08 20:53:07 +0200

    Update version

--- hivepress-paid-listings.php
+++ hivepress-paid-listings.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: HivePress Paid Listings
- * Description: Charge users for adding listings.
- * Version: 1.1.1
+ * Description: Charge users for adding, featuring and renewing listings.
+ * Version: 1.1.2
  * Author: HivePress
  * Author URI: https://hivepress.io/
  * Text Domain: hivepress-paid-listings
