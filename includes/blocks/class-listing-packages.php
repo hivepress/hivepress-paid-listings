0b03bbf75aaafb4a8534a6109ea1227d801983c5
Ihor <ihor@hivepress.io>
2020-11-25 22:37:56 +0200

    Add packages block settings

--- includes/blocks/class-listing-packages.php
+++ includes/blocks/class-listing-packages.php
@@ -21,11 +21,66 @@ defined( 'ABSPATH' ) || exit;
 class Listing_Packages extends Block {
 
 	/**
+	 * Columns number.
+	 *
+	 * @var int
+	 */
+	protected $columns;
+
+	/**
+	 * Packages number.
+	 *
+	 * @var int
+	 */
+	protected $number;
+
+	/**
 	 * Template mode.
 	 *
 	 * @var string
 	 */
 	protected $mode = 'view';
+
+	/**
+	 * Class initializer.
+	 *
+	 * @param array $meta Block meta.
+	 */
+	public static function init( $meta = [] ) {
+		$meta = hp\merge_arrays(
+			[
+				'label'    => hivepress()->translator->get_string( 'listing_packages' ),
+
+				'settings' => [
+					'columns' => [
+						'label'    => hivepress()->translator->get_string( 'columns_number' ),
+						'type'     => 'select',
+						'default'  => 3,
+						'required' => true,
+						'_order'   => 10,
+
+						'options'  => [
+							2 => '2',
+							3 => '3',
+							4 => '4',
+						],
+					],
+
+					'number'  => [
+						'label'     => hivepress()->translator->get_string( 'items_number' ),
+						'type'      => 'number',
+						'min_value' => 1,
+						'default'   => 3,
+						'required'  => true,
+						'_order'    => 20,
+					],
+				],
+			],
+			$meta
+		);
+
+		parent::init( $meta );
+	}
 
 	/**
 	 * Renders block HTML.
