d1275abed9779af984109ea354b136b535e4139a
Ihor <ihor@hivepress.io>
2020-11-26 13:12:37 +0200

    Fix strings

--- templates/listing-package/view/listing-package-select-button.php
+++ templates/listing-package/view/listing-package-select-button.php
@@ -9,9 +9,9 @@ if ( isset( $user_listing_package ) ) :
 	<button type="button" class="hp-listing-package__select-button hp-listing-package__button button button--primary alt" data-component="link" data-url="<?php echo esc_url( $listing_package_url ); ?>">
 		<?php
 		if ( $listing_package->get_product__id() ) :
-			esc_html_e( 'Buy Package', 'hivepress-paid-listings' );
+			echo esc_html( hivepress()->translator->get_string( 'buy_listing_package' ) );
 		else :
-			esc_html_e( 'Select Package', 'hivepress-paid-listings' );
+			echo esc_html( hivepress()->translator->get_string( 'select_listing_package' ) );
 		endif;
 		?>
 	</button>
