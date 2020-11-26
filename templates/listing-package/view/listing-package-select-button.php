4bc94f8bff0cc09754a48b9e5405a55f39323131
Ihor <ihor@hivepress.io>
2020-11-27 00:30:44 +0200

    Add caption wrapper

--- templates/listing-package/view/listing-package-select-button.php
+++ templates/listing-package/view/listing-package-select-button.php
@@ -4,16 +4,20 @@ defined( 'ABSPATH' ) || exit;
 
 if ( isset( $user_listing_package ) ) :
 	?>
-	<button type="button" class="hp-listing-package__select-button hp-listing-package__button button" disabled><?php echo esc_html( hivepress()->translator->get_string( 'listing_limit_exceeded' ) ); ?></button>
+	<button type="button" class="hp-listing-package__select-button hp-listing-package__button button" disabled>
+		<span><?php echo esc_html( hivepress()->translator->get_string( 'listing_limit_exceeded' ) ); ?></span>
+	</button>
 <?php else : ?>
 	<button type="button" class="hp-listing-package__select-button hp-listing-package__button button button--primary alt" data-component="link" data-url="<?php echo esc_url( $listing_package_url ); ?>">
-		<?php
-		if ( $listing_package->get_product__id() ) :
-			echo esc_html( hivepress()->translator->get_string( 'buy_listing_package' ) );
-		else :
-			echo esc_html( hivepress()->translator->get_string( 'select_listing_package' ) );
-		endif;
-		?>
+		<span>
+			<?php
+			if ( $listing_package->get_product__id() ) :
+				echo esc_html( hivepress()->translator->get_string( 'buy_listing_package' ) );
+			else :
+				echo esc_html( hivepress()->translator->get_string( 'select_listing_package' ) );
+			endif;
+			?>
+		</span>
 	</button>
 	<?php
 endif;
